793976d59dbe2617647949aa9befd42356137d2b
ravkr77751 <ravkrv1970@gmail.com>
2026-04-04 10:10:58 +0530

    Added script test

---
+++ feature1/test1.sh
@@ -0,0 +1,10 @@
+ #!/bin/bash
+title=$(curl -s https://127.0.0.1 | grep -oP '(?<=<title>)(.*)(?=</title>)')
+echo $title
+if [[ $title == "GitHubv1.1" ]]; then
+    echo "Test Successfull !!"
+    exit 0
+else
+    echo "Test Unsuccessful !!"
+    exit 1
+fi

7fdb8ed7501d8ef219f1d125bf9d4a24aa8e7072
ravkr77751 <ravkrv1970@gmail.com>
2026-04-09 09:05:20 +0530

    Update test1.sh

--- feature1/test1.sh
+++ feature1/test1.sh
@@ -1,6 +1,7 @@
  #!/bin/bash
 title=$(curl -s https://127.0.0.1 | grep -oP '(?<=<title>)(.*)(?=</title>)')
 echo $title
+echo 'Entering tests'
 if [[ $title == "GitHubv1.1" ]]; then
     echo "Test Successfull !!"
     exit 0
